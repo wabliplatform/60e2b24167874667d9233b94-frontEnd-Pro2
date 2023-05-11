5507c7fdb7d286e092b130712c5085267008c8e1
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2023-05-11 16:57:01 +0300

    My commit message

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+	'Home' : './javascript/Home.js',
+	'mcandidates' : './javascript/mcandidates.js',
+	'allcandidates' : './javascript/allcandidates.js',
+	'candidate' : './javascript/candidate.js',
+	'cmunicipality' : './javascript/cmunicipality.js',
+	'upmunicipality' : './javascript/upmunicipality.js',
+	'cconsultant' : './javascript/cconsultant.js',
+	'ccandidate' : './javascript/ccandidate.js',
+	'consultant' : './javascript/consultant.js',
+	'Page10' : './javascript/Page10.js',
+},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    
+  },
+};
